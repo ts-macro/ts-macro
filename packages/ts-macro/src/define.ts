9c9be07742fb45d5a3d20bebb0bb26372269dbe8
zhiyuanzmj <260480378@qq.com>
2025-03-05 15:25:10 +0800

    feat(ts-macro): support nested plugins for @vue/language-tools

--- packages/ts-macro/src/define.ts
+++ packages/ts-macro/src/define.ts
@@ -12,39 +12,40 @@ export function createPlugin(factory: any) {
   return (options: any) => {
     // compatible with @vue/language-tools
     if (options?.modules) {
-      return [
-        factory({
-          ts: options.modules.typescript,
-          ...options,
-        }),
-      ].flatMap((result) => {
-        if (result.resolveVirtualCode) {
-          result.resolveEmbeddedCode ??= (
-            filePath: string,
-            sfc: Sfc,
-            embeddedFile: VueEmbeddedCode,
-          ) => {
-            for (const source of ['script', 'scriptSetup'] as const) {
-              const ast = sfc[source]?.ast
-              if (!ast) continue
-              result.resolveVirtualCode({
-                ast,
-                source,
-                filePath,
-                id: embeddedFile.id,
-                codes: embeddedFile.content,
-                languageId: embeddedFile.lang,
-                embeddedCodes: embeddedFile.embeddedCodes,
-                linkedCodeMappings: embeddedFile.linkedCodeMappings,
-              })
+      const plugins = factory({
+        ts: options.modules.typescript,
+        ...options,
+      })
+      return (Array.isArray(plugins) ? plugins : [plugins]).flatMap(
+        (result) => {
+          if (result.resolveVirtualCode) {
+            result.resolveEmbeddedCode ??= (
+              filePath: string,
+              sfc: Sfc,
+              embeddedFile: VueEmbeddedCode,
+            ) => {
+              for (const source of ['script', 'scriptSetup'] as const) {
+                const ast = sfc[source]?.ast
+                if (!ast) continue
+                result.resolveVirtualCode({
+                  ast,
+                  source,
+                  filePath,
+                  id: embeddedFile.id,
+                  codes: embeddedFile.content,
+                  languageId: embeddedFile.lang,
+                  embeddedCodes: embeddedFile.embeddedCodes,
+                  linkedCodeMappings: embeddedFile.linkedCodeMappings,
+                })
+              }
             }
           }
-        }
-        result.order ??=
-          result.enforce === 'pre' ? -1 : result.enforce === 'post' ? 1 : 0
-        result.version ??= 2.1
-        return result
-      })
+          result.order ??=
+            result.enforce === 'pre' ? -1 : result.enforce === 'post' ? 1 : 0
+          result.version ??= 2.1
+          return result
+        },
+      )
     }
     return (context: any) => factory(context, options)
   }
