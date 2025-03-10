d6f1a42df0211c1e2a84b099776761a04a1d4254
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-11 00:18:06 +0800

    chore: upgrade eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,7 @@
 import { sxzz } from '@sxzz/eslint-config'
 
-export default sxzz({
-  rules: {
-    'unicorn/prefer-array-find': 'off',
-    'array-callback-return': 'off',
-    '@typescript-eslint/no-require-imports': 'off',
-  },
-})
+export default sxzz().removeRules(
+  'unicorn/prefer-array-find',
+  'array-callback-return',
+  '@typescript-eslint/no-require-imports',
+)
