22be6848463a28e4d869ce0a96871d16acf3efe8
zhiyuanzmj <260480378@qq.com>
2025-06-05 18:43:33 +0800

    feat(language-plugin): enable setParentNodes for createSourceFile

--- packages/language-plugin/src/index.ts
+++ packages/language-plugin/src/index.ts
@@ -54,6 +54,7 @@ export const getLanguagePlugins = (
             `index.${languageId}`,
             snapshot.getText(0, snapshot.getLength()).toString(),
             99 satisfies typeof ts.ScriptTarget.Latest,
+            true
           )
           return new TsmVirtualCode(filePath, ast, languageId, resolvedPlugins)
         }
